565d46ed76089a02e1d7e48ddec2adedfcc81aba
annatakman <annatakman@gmail.com>
2020-07-12 19:27:33 +0200

    fetch of characters from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import styled from 'styled-components'
 import {CharacterList} from './pages/CharacterList'
 import {CharacterPage} from './pages/CharacterPage'
 import {Footer} from './components/Footer'
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
+const MainContent = styled.div`
+  flex: 1;
+`
+
 function App() {
   return (
     <BrowserRouter>
+    <Wrapper>
+      <MainContent>
     <Switch>
       <Route path="/" exact>
         <CharacterList />
@@ -15,7 +28,9 @@ function App() {
         <CharacterPage />
       </Route>
     </Switch>
+    </MainContent>
     <Footer />
+    </Wrapper>
   </BrowserRouter>
   )
 }
--- src/pages/CharacterList.js
+++ src/pages/CharacterList.js
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from 'react'
+import md5 from 'md5'
 import { Link } from 'react-router-dom'
 
 export const CharacterList = () => {
   const [characters, setCharacters] = useState([])
+  const timestamp = Date.now().toString()
+  const hash = md5(timestamp + process.env.REACT_APP_PRIVATE_KEY + '785f108ac49ee15d64e2d1c53999f652')
 
   useEffect(() => {
-    fetch('')
+    fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=785f108ac49ee15d64e2d1c53999f652&hash=${hash}`)
       .then(res => res.json())
       .then(json => {
-        setCharacters(json)
+        setCharacters(json.data.results)
       })
-  }, [])
+  })
 
-  return(
-  <div>Marvel</div>
+  //console.log(process.env.REACT_APP_PRIVATE_KEY)
+  //console.log(timestamp)
+  //console.log(md5(timestamp + process.env.REACT_APP_PRIVATE_KEY + '785f108ac49ee15d64e2d1c53999f652'))
+
+
+  return (
+    <section>
+
+      {characters.map(character => (
+        <p key={character.id}>{character.name}</p>
+      ))}
+    </section>
   )
 }
