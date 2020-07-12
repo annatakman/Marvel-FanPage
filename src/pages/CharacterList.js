bf3ab4d6762d29bfb8f765eea6225473940892f5
annatakman <annatakman@gmail.com>
2020-07-12 19:48:46 +0200

    changed useEffect dependencies

--- src/pages/CharacterList.js
+++ src/pages/CharacterList.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom'
 export const CharacterList = () => {
   const [characters, setCharacters] = useState([])
   const timestamp = Date.now().toString()
-  const hash = md5(timestamp + process.env.REACT_APP_PRIVATE_KEY + '785f108ac49ee15d64e2d1c53999f652')
+  const publicKey = '785f108ac49ee15d64e2d1c53999f652'
+  const hash = md5(timestamp + process.env.REACT_APP_PRIVATE_KEY + publicKey)
 
   useEffect(() => {
     fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=785f108ac49ee15d64e2d1c53999f652&hash=${hash}`)
@@ -13,7 +14,7 @@ export const CharacterList = () => {
       .then(json => {
         setCharacters(json.data.results)
       })
-  })
+  }, [])
 
   //console.log(process.env.REACT_APP_PRIVATE_KEY)
   //console.log(timestamp)
